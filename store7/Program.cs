c0454ffd9c377d6483e6140a568e7c1ff69f3754
Andrey Namestnikov <andrewnames85@gmail.com>
2021-06-16 13:42:42 -0400

    Update Program.cs

--- store7/Program.cs
+++ store7/Program.cs
@@ -1,12 +1,265 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace store7
+namespace KVstore
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var storage = new DuplicableStorage();
+            bool endApp = false;
+
+            Console.WriteLine("Storage APP\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                string[] inputArgs = Console.ReadLine().Split(" ");
+                var commandName = inputArgs[0];
+                try
+                {
+
+                    switch (commandName)
+                    {
+
+                        case "exit":
+                            endApp = true;
+                            break;
+
+                        case "add":
+                            storage.Add(inputArgs[1], inputArgs[2]);
+                            break;
+                        case "remove":
+                            storage.Remove(inputArgs[1], inputArgs[2]);
+                            break;
+                        case "keys":
+                            storage.Keys();
+                            break;
+                        case "members":
+                            storage.Members(inputArgs[1]);
+                            break;
+
+                        case "removeAll":
+                            storage.RemoveAll(inputArgs[1]);
+                            break;
+
+                        case "clear":
+                            storage.Clear();
+                            break;
+
+                        case "keyExist":
+                            storage.KeyExist(inputArgs[1]);
+                            break;
+
+                        case "allMembers":
+                            storage.AllMembers();
+                            break;
+                        case "items":
+                            storage.Items();
+                            break;
+                        default:
+                            Console.WriteLine("Command does not exist");
+                            break;
+
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    if (e is IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Missing parameter");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+
         }
+        // if we only need unique keys in the storage
+        class Storage
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            public void Add(string key, string value)
+            {
+                var result = dictionary.TryAdd(key, value);
+                Console.WriteLine(result ? "Added" : "ERROR, member already exists for key");
+            }
+
+
+            public void Remove(string key)
+            {
+                var result = dictionary.Remove(key);
+                Console.WriteLine(result ? "Removed" : "ERROR, member does not exist");
+            }
+
+            public void Keys()
+            {
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+
+            public void Members(string key)
+            {
+                var kvps = dictionary.Where(kvp => kvp.Key == key).ToList();
+                if (kvps.Count == 0)
+                {
+                    Console.WriteLine("ERROR, key does not exist.");
+                    return;
+                }
+
+                foreach (var kvp in kvps)
+                {
+                    Console.WriteLine(kvp.Value);
+                }
+            }
+
+
+            public void RemoveAll(string key)
+            {
+                if (dictionary.ContainsKey(key))
+                {
+                    foreach (var kvp in dictionary.Where(kvp => kvp.Key == key).ToList())
+                    {
+                        dictionary.Remove(kvp.Key);
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR, key does not exist.");
+                }
+            }
+
+            public void Clear()
+            {
+                dictionary.Clear();
+                Console.WriteLine("Storage Cleared");
+
+            }
+
+            public void KeyExist(string key)
+            {
+                var result = dictionary.ContainsKey(key);
+                Console.WriteLine(result);
+            }
+
+            public void AllMembers()
+            {
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine(kvp.Value);
+                }
+            }
+            public void Items()
+            {
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine("{0} : {1}", kvp.Key, kvp.Value);
+                }
+            }
+        }
+        // if we want to allow non unique keys in the storage
+        class DuplicableStorage
+        {
+            List<KeyValuePair<string, string>> dictionary = new List<KeyValuePair<string, string>>();
+
+            public void Add(string key, string value)
+            {
+                var newPair = new KeyValuePair<string, string>(key, value);
+                var checkup = dictionary.Contains(newPair);
+                if (!checkup)
+                {
+                    dictionary.Add(newPair);
+                }
+
+                Console.WriteLine(!checkup ? "Added" : "ERROR, member already exists for key");
+            }
+
+
+            public void Remove(string key, string value)
+            { 
+
+                var result = dictionary.Remove(dictionary.Where(kvp => kvp.Key == key && kvp.Value == value).FirstOrDefault());
+                Console.WriteLine(result ? "Removed" : "ERROR, member does not exist");
+            }
+
+            public void Keys()
+            {
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine(kvp.Key);
+                }
+            }
+
+            public void Members(string key)
+            {
+                var kvps = dictionary.Where(kvp => kvp.Key == key).ToList();
+                if (kvps.Count == 0)
+                {
+                    Console.WriteLine("ERROR, key does not exist.");
+                    return;
+                }
+
+                foreach (var kvp in kvps)
+                {
+                    Console.WriteLine(kvp.Value);
+                }
+            }
+
+
+            public void RemoveAll(string key)
+            {
+                if (dictionary.Any(kvp => kvp.Key == key))
+                {
+                    dictionary.RemoveAll(kvp => kvp.Key == key);
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR, key does not exist.");
+                }
+            }
+
+            public void Clear()
+            {
+                dictionary.Clear();
+                Console.WriteLine("Storage Cleared");
+
+            }
+
+            public void KeyExist(string key)
+            {
+                var result = dictionary.Any(kvp => kvp.Key == key);
+                Console.WriteLine(result);
+            }
+
+            public void AllMembers()
+            {
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine(kvp.Value);
+                }
+            }
+
+            public void Items()
+            {
+                foreach (KeyValuePair<string, string> kvp in dictionary)
+                {
+                    Console.WriteLine("{0} : {1}", kvp.Key, kvp.Value);
+                }
+            }
+        }
+
     }
+
+
 }
